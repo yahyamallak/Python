290c2d1c42953f97223490d12a5d9998cc9250eb
Yahya mallak <Yahya10mallak@gmail.com>
2022-01-07 19:23:36 +0100

    python numbers

---
+++ basics/numbers.py
@@ -0,0 +1,6 @@
+number1 = 5
+number2 = 10
+result = number1 + number2
+
+print(str(result) + " is the sum of the two numbers")
+
---
+++ basics/strings.py
@@ -0,0 +1,17 @@
+text = "Hi guys, hope you're good"
+
+
+# string length
+print(len(text))
+
+# string to upper
+print(text.upper())
+
+# is string lower
+print(text.islower())
+
+# where is the position of U
+print(text.index("u"))
+
+# replace guys with there
+print(text.replace("guys", "There"))
