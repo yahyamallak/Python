a73aa3bfdd1d3dfff52897a5bb35fae84be4f5e2
Yahya mallak <Yahya10mallak@gmail.com>
2022-01-08 00:32:24 +0100

    python functions

---
+++ basics/function.py
@@ -0,0 +1,13 @@
+def sayHello():
+    print("Hello there!")
+
+def sum(num1, num2):
+    print(num1 + num2)
+
+def mult(num1, num2):
+    return num1 * num2
+
+sayHello()
+sum(2,3)
+print(mult(5,2))
+
