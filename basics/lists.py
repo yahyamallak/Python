9fb00168386c54bdfcc9ae9b02c5d24489e5cec5
Yahya mallak <Yahya10mallak@gmail.com>
2022-01-07 20:03:53 +0100

    python lists

---
+++ basics/lists.py
@@ -0,0 +1,20 @@
+players = ["messi", "ibra", "neymar", "xavi"]
+
+print(players[0])
+
+players[1] = "iniesta"
+
+players.remove("messi")
+
+players.append("suarez")
+
+print(players[0:])
+
+players.insert(0,"buffon")
+
+players.pop()
+
+print(players[0:2])
+
+
+print(players[0:])
